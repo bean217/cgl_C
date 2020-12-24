3e4589fb30a376abfba37d08c40824876a82b9c8
Ben Piro <benpiro1118@gmail.com>
2020-12-24 16:48:02 -0500

    Able to get the mouse position, but cannot get position past column index 94. Also have to use left click to get the position event update for some reason (not sure why).

--- cursed_life.c
+++ cursed_life.c
@@ -4,17 +4,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <signal.h>
 #include "cgl.h"
 #include "curse_lib.h"
 
 static WINDOW * win;
+static MEVENT event;
 
 void init() {
 	initscr(); // initializes stdscr
         cbreak(); // allows CTRL-C to escape stdscr
         noecho(); // disable character echoing
 	curs_set(0); // hide default cursor
+	keypad(stdscr, TRUE);
 	refresh();
 }
 
@@ -23,9 +24,18 @@ void endit() {
 }
 
 void draw() {
+	werase(win);
 	box(win, 0, 0);
 	mvwprintw(win, 1, 1, "COLS = %d, LINES = %d", COLS, LINES);
-	
+
+	if (getmouse(&event) == OK) {
+		wmouse_trafo(win, &event.y, &event.x, TRUE);
+		mvwprintw(win,2, 1, "COOR X = %d, COOR Y = %d", event.x, event.y);
+		mvwaddch(win, event.y, event.x, ACS_DIAMOND);
+	} else {
+		mvwprintw(win, 2, 1, "NO MOUSE EVENT");
+	}
+
 	/*
 	char *str = "Game of Life";
 	int y, x;
@@ -46,73 +56,25 @@ void draw() {
 int main(void) {
 	init();
 	win = newwin(LINES, COLS, 0, 0);
+	
+	mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
+	
 	draw();
 
 	int key;
 	while ((key = getch()) != 'q') {
 		if (key == KEY_RESIZE) {
+			// reset stdscr
 			endwin();
 			refresh();
-			
-			werase(win);
-			wresize(win, (LINES < 25) ? 25 : LINES, (COLS < 75) ? 75 : COLS);
-			
-			draw();
+			// reset stdscr subwindow and resize it
+			//werase(win);
+			wresize(win, (LINES < 25) ? 25 : LINES, (COLS < 75) ? 75 : COLS);	
 		}
-		
+		// draw the window
+		draw();	
 
 	}
-
-	/*
-	WINDOW *win;
-	
-	int height = 3;
-	int width = 10;
-	int starty = (LINES - height) / 2;
-	int startx = (COLS - width) / 2;
-	refresh();
-	win = create_win(height, width, starty, startx);
-	int oldLines = LINES;
-	int oldCols = COLS;
-	while (1) {
-		if (LINES != oldLines || COLS != oldCols) {
-			resizeterm(LINES, COLS);	
-		}
-		oldLines = LINES;
-		oldCols = COLS;
-		starty = (LINES - height) / 2;
-		startx = (COLS - height) / 2;
-		mvwin(win, starty, startx);
-		wrefresh(win);
-		refresh();
-		if (getch() == 'k') break;
-	}
-*/	
-
-	/*
-	int rows, cols, i, j;
-	rows = 40;
-	cols = 80;
-	Cell grid[rows][cols];
-
-	char start[rows][cols];
-
-	for (j = 0; j < rows; j++) {
-		for (i = 0; i < cols; i++) {
-			start[j][i] = 0;
-		}
-	}
-
-	init_cgl(rows, cols, start, grid);
-
-	for (j = 0; j < rows; j++) {
-		for (i = 0; i < cols; i++) {
-			if (grid[j][i].live_state != 0) printf("*");
-			else printf(" ");
-		}
-		printf("\n");
-	}
-	*/
 
 	endit();	
 }
